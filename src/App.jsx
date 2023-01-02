e5cf1bf5ad6cd7a8fa2b7f59ce829709e2d1e69c
Shajadul <shajadulrahe@gmail.com>
2023-01-02 09:37:17 +0600

    Update: Better data fetching with react-router-6.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
-import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
-import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
-import WelcomePage from './pages/Welcome';
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage from "./pages/BlogPosts";
+import NewPostPage from "./pages/NewPost";
+import PostDetailPage from "./pages/PostDetail";
+import RootLayout from "./components/RootLayout";
+import WelcomePage from "./pages/Welcome";
+import { loader as blogLoader } from "./pages/BlogPosts";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="/blog" element={<BlogLayout />}>
+          <Route index element={<BlogPostsPage />} loader={blogLoader} />
+          <Route path=":id" element={<PostDetailPage />} />
+        </Route>
+        <Route path="/blog/new" element={<NewPostPage />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
